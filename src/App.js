75346d888d4014d6c9be65aec9ef3214c8e906b5
sobkovayulia <sssobkova@gmail.com>
2021-08-17 06:40:00 +0200

    summary...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-<<<<<<< Updated upstream
-import 'primeflex/primeflex.css';
-import './App.css';
-import 'primereact/resources/themes/md-light-indigo/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
-import { DataTableDynamicDemo } from './Demo';
-
-=======
 import React from 'react';
 import { DataTableBasicDemo } from './demo/DataTableBasicDemo';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <DataTableDynamicDemo/>
-=======
       <DataTableBasicDemo />
->>>>>>> Stashed changes
     </div>
   );
 }
